a7684afc57b532481029b4befb24f013630f08e8
Kien_T <waguri-kaoruko.husband@gmail.com>
2025-11-23 21:31:23 +0700

    dd

--- 33_Search_in_Rotated_Sorted_Array/solution.py
+++ 33_Search_in_Rotated_Sorted_Array/solution.py
@@ -97,30 +97,27 @@ def solve():
     print("\n".join(out))
 
 # solution Function
-def search(nums, target):
+def search(self, nums, target):
     if not nums:
         return 0
-    
     l, r = 0, len(nums) - 1
-    
-    while l <= r:
+    while (l <= r):
         mid = l + (r - l) // 2
-        
         if nums[mid] == target:
             return mid
-        if nums[l] <= nums[mid]:
-            if nums[l] <= target <= nums[mid]:
+        #Binary
+        if nums[mid] >= nums[l]:
+            if target >= nums[l] and target <= nums[mid]:
                 r = mid - 1
             else:
                 l = mid + 1
-                
         else:
-            if nums[mid] <= target <= nums[r]:
+            if target >= nums[mid] and target <= nums[r]:
                 l = mid + 1
             else:
                 r = mid - 1
-                
-    return -1
+    return -1 
+
 
 # =========================== ENTRY ===========================
 if __name__ == "__main__":
