698a3484ee0621c0aac27e58c57c59e5dc8b72e1
Kien_T <waguri-kaoruko.husband@gmail.com>
2025-12-06 21:52:40 +0700

    d

--- 191_Number_of_1_Bits/solution.py
+++ 191_Number_of_1_Bits/solution.py
@@ -99,10 +99,13 @@ def solve():
 # solution Function
 def hammingWeight(self, n: int) -> int:
     counter = 0
-    mask = 0xFFFFFFFF
+    # mask = 0xFFFFFFFF
+    # while n:
+    #     counter += n % 2
+    #     n = n >> 1
     while n:
-        counter += n % 2
-        n = n >> 1
+        n &= (n - 1)
+        counter += 1
     return counter
 # =========================== ENTRY ===========================
 if __name__ == "__main__":
